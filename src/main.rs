eaf4341e227fd96deacc37fe65f8ae91a4c06dc8
Gergely Brautigam <skarlso777@gmail.com>
2018-06-25 15:32:17 +0200

    Added command handling.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let _matches = App::from_yaml(yaml).get_matches();
 }
