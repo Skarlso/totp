ff42f2b6294d756d8003a953604a4ddcb1651f6b
Gergely Brautigam <skarlso777@gmail.com>
2018-06-29 06:53:35 +0200

    Creating an account.

--- src/commands.rs
+++ src/commands.rs
@@ -1,13 +1,20 @@
 extern crate clap;
+use std::io::{self, Read};
 
 pub fn add_account() {
+    println!("Adding account");
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
 
+    handle.read_to_string(&mut buffer)?;
+    Ok(())
 }
 
-pub fn delete_account() {
-
+pub fn delete_account(account: &str) {
+    println!("Deleting account {}", account);
 }
 
-pub fn generate_token(args: &clap::ArgMatches<'_>) {
-    println!("{:?}", args.value_of("account").unwrap());
+pub fn generate_token(account: &str) {
+    println!("{:?}", account);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate clap;
+use clap::App;
 
 mod generator;
 mod encrypt;
@@ -7,12 +8,22 @@ mod accounting;
 mod commands;
 
 fn main() {
-    use clap::App;
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
     match matches.subcommand() {
         ("add", Some(_)) => commands::add_account(),
-        ("generate", Some(token)) => commands::generate_token(token),
+        ("generate", Some(token)) => {
+            match token.value_of("account") {
+                Some(acc) => commands::generate_token(acc),
+                None => println!("Please define an --account to generate a token for"),
+            }
+        },
+        ("delete", Some(token)) => {
+            match token.value_of("account") {
+                Some(acc) => commands::delete_account(acc),
+                None => println!("Please define an --account to delete"),
+            }
+        },
         _ => {},
     }
 }
